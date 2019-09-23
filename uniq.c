f4abcb0fea4c16b123c781d997e39273212dfe41
Reuben Varghese <reubenvarghese1@gmail.com>
2019-09-22 21:11:01 -0400

    Testing after optimization

--- uniq.c
+++ uniq.c
@@ -188,13 +188,11 @@ load(int fd,int qt,int cflag,int dflag,int iflag)
     int next=0;
 
     //The following arrays are to store the ascii values for the d flag
+    int alreadyusedlines[1] = {0};
+    int alreadyusedlinesprinted[1] = {0};
     if(dflag ==1){
         int *alreadyusedlines = malloc( sizeof(int) * linescount);
         int *alreadyusedlinesprinted = malloc( sizeof(int) * linescount/3);
-    }
-    else{
-        int alreadyusedlines[1] = {0};
-        int alreadyusedlinesprinted[1] = {0};
     }
     int dupnumber = 0;
     int printedlinenumber=0;
